7f3be4a16305cec57cb7a307cf42e07f5e5dcf42
lonelysun21 <sanzhar700.com@gmail.com>
2026-08-22 14:44:29 +0500

    Create main.go with variable declarations and output
    Initial implementation of a Go program with variable declarations and print statements.

---
+++ August-22-2026/:=/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+    // Переменные, объявленные с явными типами
+    var name string = "Gopher"
+    var age int = 5
+
+    // TODO: Замените строку ниже на объявление := переменной 'language', используя значение, которое требует задание
+    language := "Go"
+
+    // TODO: Замените строку ниже на объявление := переменной 'version', используя значение, которое требует задание
+    version := 1.18
+
+    // Вывести все переменные
+    fmt.Println("Name:", name)
+    fmt.Println("Age:", age)
+    fmt.Println("Language:", language)
+    fmt.Println("Version:", version)
+}
