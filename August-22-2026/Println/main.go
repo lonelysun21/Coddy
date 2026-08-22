8a2e584f75f12b325a6b95217de39015359837d4
lonelysun21 <sanzhar700.com@gmail.com>
2026-08-22 14:28:52 +0500

    Implement main function to print user info

---
+++ August-22-2026/Println/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	// TODO: Используйте fmt.Println(), чтобы вывести "Alex 25 coding" в одну строку
+	// Подсказка: Вы можете передать несколько аргументов в fmt.Println()
+	fmt.Println("Alex", "25", "coding")
+}
