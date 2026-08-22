c561541e8fa778e22d9436100b756f26b838825e
lonelysun21 <sanzhar700.com@gmail.com>
2026-08-22 14:35:50 +0500

    Add main.go with a simple Hello World program

---
+++ August-22-2026/Comments/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+    // Введите свой код ниже
+    // Превратите строку ниже в комментарий, чтобы она перестала выполняться
+    //fmt.Println("Goodbye!")
+    fmt.Println("Hello, Go!")
+}
