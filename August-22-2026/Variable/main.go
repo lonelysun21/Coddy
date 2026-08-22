2c6b0bb37a1cef46e90cd870e027ea1c8d92c6c1
lonelysun21 <sanzhar700.com@gmail.com>
2026-08-22 14:39:43 +0500

    Initialize name and age variables in main.go

---
+++ August-22-2026/Variable/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+    // TODO: Объявите переменную с именем 'name' типа string с помощью var, со значением, которое требуется в задании
+    var name string = "Gopher"
+
+    // TODO: Объявите переменную с именем 'age' типа int с помощью var, со значением, которое требуется в задании
+    var age int = 5
+
+    // Не изменяйте код ниже
+    fmt.Println("My name is", name, "and I am", age, "years old.")
+}
