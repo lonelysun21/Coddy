82c862936a294b7540ce1edaa06710e07b2ae21d
lonelysun21 <sanzhar700.com@gmail.com>
2026-09-09 22:08:55 +0500

    Create main.go

---
+++ September-09-2026/"for" loop/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	// TODO: Напишите цикл for, который считает от 1 до 10
+	// и выводит каждое число на новой строке
+	for i := 1; i <= 10; i++ {
+		fmt.Println(i)
+	}
+}
