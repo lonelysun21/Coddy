6c81eaa4109c369184efd2f8fa3d8c83e610854e
lonelysun21 <sanzhar700.com@gmail.com>
2026-09-18 11:00:16 +0500

    Create main.go

---
+++ September-18-2026/Introduction to Arrays/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+    // TODO: Объявите массив с именем 'numbers', который содержит 5 целых чисел: 10, 20, 30, 40 и 50
+    numbers := [5]int{10, 20, 30, 40, 50}
+    // Это выведет ваш массив
+    fmt.Println("My array of numbers:", numbers)
+}
